abee51873d9e11650d4e790428af1ccdaefadc51
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-21 00:56:23 +0530

    arrays

---
+++ array.php
@@ -0,0 +1,20 @@
+//index array
+<?php
+$a=array("welcome",1,1.3,true);
+echo $a[3]."</br>";
+echo count($a)
+?>
+
+//associative array- it is a key value pair
+<?php
+$marks["tony"]=97;
+$marks["aditya"]=56;
+$marks["sanskar"]=90;
+echo $marks['tony'];
+?>
+
+//multidimensional array
+<?php
+$array=array(array(1,2,3),array(4,5,6));
+echo $array;
+?>
