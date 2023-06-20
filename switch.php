b1fcd4f3d2b492e934454214db456ba7a8b3551a
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-20 23:40:46 +0530

    switch statement

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+$x=15;
+switch($x){
+    case 1:
+        echo"the number is one";
+        break;
+    case 15:
+        echo"the number is 15";
+        break;
+    default:
+        echo"other value";
+}
+?>
+    
